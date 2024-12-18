a144cfd5fc79e53095ed75420c552b380943e76a
Отмахов Дмитрий <Dmitriy.Otmahov@fabit.ru>
2024-12-18 11:58:45 +0700

    finish 159

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,14 @@
-import { Button, Htag, Input, P, Rating, Tag, Textarea } from '@/components';
+import { Htag } from '@/components';
 import { withLayout } from '@/layout/Layout';
 import { GetStaticProps } from 'next';
-import { useState } from 'react';
 import axios from 'axios';
 import { MenuItem } from '@/interfaces/menu.interface';
 import { API } from '@/helpers/api';
 
 function Home() {
-  const [rating, setRating] = useState(4);
-
   return (
     <>
-      <Htag tag='h1'>123</Htag>
-      <Button appearance='primary' className='123' arrow='down'>
-        Кнопка
-      </Button>
-      <Button appearance='ghost' arrow='right'>
-        Кнопка 2
-      </Button>
-      <P size='s'>Большой</P>
-      <P>Средний</P>
-      <P size='l'>Маленький</P>
-      <Tag size='s'>def</Tag>
-      <Tag size='m' color='primary'>
-        primary
-      </Tag>
-      <Tag size='s' color='gray'>
-        gray
-      </Tag>
-      <Tag size='s' color='red'>
-        red
-      </Tag>
-      <Tag size='s' color='green'>
-        green
-      </Tag>
-      <Rating rating={rating} isEditable={true} setRating={setRating}></Rating>
-      <Input placeholder='Тест' />
-      <Textarea placeholder='Тест' />
+      <Htag tag='h1'>Тест</Htag>
     </>
   );
 }
